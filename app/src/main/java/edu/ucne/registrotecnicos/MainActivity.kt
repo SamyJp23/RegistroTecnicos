c4172b4bf22f751d72a1f832459420358fe239f8
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-01-14 00:18:05 -0400

    Creando el Dao

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             RegistroTecnicosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    @Entity(tableName = "Tecnicos")
+    data class TecnicoEntity(
+        @PrimaryKey
+        val tecnicoId: Int? = null,
+        val nombre: String = "",
+        val sueldo: Double
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroTecnicosTheme {
-        Greeting("Android")
+
     }
-}
+
+
+
+
