43ee372f7e8fe3aea98c70e16d3f0f423c60dbc2
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-01-17 12:27:26 -0400

    Creando el archivo Screen, TecnicoListScreen, TecnicoScreen y eliminando el archivo TecicoDAo

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/dao/TecnicoDao.kt
+++
@@ -1,2 +0,0 @@
-package edu.ucne.registrotecnicos.data.local.dao
-
---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package edu.ucne.registrotecnicos.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen{
+
+
+}
