74941b8c9eb4f3112af94d48c4ba247b8f3364e3
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-01-17 16:54:28 -0400

    Creando la clase Screen

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -3,6 +3,9 @@ package edu.ucne.registrotecnicos.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen{
-
+@Serializable
+data object TecnicoList : Screen()
+    @Serializable
+    data class Tecnico(val tecnicoId: Int) : Screen()
 
 }
